9679708cd546940b81cb3e2c837715606e7faf65
Stefano Driussi <stedri@hotmail.it>
2017-03-15 15:18:42 +0100

    Add test project with first failing test :"

---
+++ TestProject/StringTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringOccurenciesCounter;
+
+namespace TestProject
+{
+	[TestClass]
+	public class StringTests
+	{
+		[TestMethod]
+		public void Result_Should_Be_The_Word_Known()
+		{
+			// Arrange
+			var input = "If you know the enemy and know yourself you need not fear the results of a hundred battles";
+
+			// Act
+			var firstWord = OccurenciesCounter.GetFirstMaxOccurringWord(input);
+
+			// Assert
+			Assert.AreEqual("know", firstWord);
+		}
+	}
+}
