78409b460dc60de1e906112871226b613efb6a2b
Stefano Driussi <stedri@hotmail.it>
2017-03-15 15:15:57 +0100

    First attempt to solve the problem

--- StringOccurenciesCounter/OccurenciesCounter.cs
+++ StringOccurenciesCounter/OccurenciesCounter.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace StringOccurenciesCounter
 {
-    public class Class1
+    public class OccurenciesCounter
     {
+		public static string GetFirstMaxOccurringWord(string input)
+		{
+			var result = input.Split(' ')
+				.GroupBy(w => w, (k, l) => new { Key = k, Count = l.Count() })
+				.OrderByDescending(o => o.Count).FirstOrDefault()?.Key ?? string.Empty;
+			return result;
+		}
     }
 }
