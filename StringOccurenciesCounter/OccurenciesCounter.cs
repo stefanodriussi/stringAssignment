bd46e618a57fdcf2b94dcc4b516c59477814f8bf
Stefano Driussi <stedri@hotmail.it>
2017-03-15 15:21:50 +0100

    Add new implementation and fix test methods

--- StringOccurenciesCounter/OccurenciesCounter.cs
+++ StringOccurenciesCounter/OccurenciesCounter.cs
@@ -15,5 +15,29 @@ namespace StringOccurenciesCounter
 				.OrderByDescending(o => o.Count).FirstOrDefault()?.Key ?? string.Empty;
 			return result;
 		}
-    }
+
+		public static string GetFirstMaxOccurringWordRedux(string input)
+		{
+			var maxOccurencies = 0;
+			var firstMaxWord = string.Empty;
+			var bucket = new Dictionary<string, int>();
+			foreach (var currentWord in input.Split(' '))
+			{
+				if (!bucket.ContainsKey(currentWord))
+				{
+					bucket.Add(currentWord, 1);
+				}
+				else
+				{
+					bucket[currentWord] += 1;
+				}
+				if (bucket[currentWord] > maxOccurencies)
+				{
+					maxOccurencies = bucket[currentWord];
+					firstMaxWord = currentWord;
+				}
+			}
+			return firstMaxWord;
+		}
+	}
 }
--- TestProject/StringTests.cs
+++ TestProject/StringTests.cs
@@ -8,13 +8,26 @@ namespace TestProject
 	public class StringTests
 	{
 		[TestMethod]
-		public void Result_Should_Be_The_Word_Known()
+		public void Result_Should_Not_Be_The_Word_Known()
 		{
 			// Arrange
 			var input = "If you know the enemy and know yourself you need not fear the results of a hundred battles";
 
 			// Act
 			var firstWord = OccurenciesCounter.GetFirstMaxOccurringWord(input);
+
+			// Assert
+			Assert.AreNotEqual("know", firstWord);
+		}
+
+		[TestMethod]
+		public void Result_Should_Be_The_Word_Known()
+		{
+			// Arrange
+			var input = "If you know the enemy and know yourself you need not fear the results of a hundred battles";
+
+			// Act
+			var firstWord = OccurenciesCounter.GetFirstMaxOccurringWordRedux(input);
 
 			// Assert
 			Assert.AreEqual("know", firstWord);
